487deb92e74c8956808af66b9a4ad61032f133ee
vittorio96 <vittoriomaria.denti@mail.polimi.it>
2019-12-19 09:44:10 +0100

    Setup code

---
+++ PongAgent.py
@@ -0,0 +1,17 @@
+
+#Class that defines the Deep Q learning implementation of the pong game
+class PongAgent():
+
+    def _init_(self):
+        print("Hello")
+
+
+
+
+# Main function to control the agent
+def main():
+    agent = PongAgent()
+
+
+if __name__ == "__main__":
+    main()
