1236f792501751480e8a332931a55f93a603c1e3
vittorio96 <vittoriomaria.denti@mail.polimi.it>
2019-12-22 14:35:06 +0100

    Improve movement detection in preprocessing

--- PongAgent.py
+++ PongAgent.py
@@ -2,33 +2,31 @@ import random
 import gym
 import keras
 import numpy as np
+from scipy.misc import toimage
 
 #Class that defines the Deep Q learning implementation of the pong game
 class PongAgent(object):
 
     def __init__(self):
         self.env = gym.make("Pong-v0")
-        self.max_episodes = 50 #number of episodes (games) to play during training
+        self.max_episodes = 1 #number of episodes (games) to play during training
         self.num_actions = 6
-        self.epsilon = 0.5
-        self.epsilon_decay = 0.98
-        self.experience_buffer_size = 800
+        self.frames_to_merge = 4
+        self.epsilon = 0.8
+        self.epsilon_decay = 0.96
+        self.experience_buffer_size = 2000
         self.mini_batch_size = int(self.experience_buffer_size * 0.5)
-        self.update_rate = 0.8
+        self.update_rate = 0.7
         self.update_rate_decay = 0.98
         self.experience_buffer = []
         self.gamma = 0.8
-        self.model = self.build_deep_rl_model()
+        self.prediction_model = self.build_deep_rl_model()
+        self.target_model = self.build_deep_rl_model()
 
     def frame_preprocessing(self, frame_img):
         # Remove redundant pixels from the image (e.g. points and white line) and downsample with a factor of two
         frame_img = frame_img[35:195]
         frame_img = frame_img[0::2, 0::2, 0] #takes one element every two elements
-        # Create a uniform background color
-        frame_img[frame_img == 144] = 0
-        frame_img[frame_img == 109] = 0
-        # Set balls and paddles to the same color
-        frame_img[frame_img != 0] = 1
 
         return frame_img #80x80 numpy array
 
@@ -49,8 +47,8 @@ class PongAgent(object):
 
         # Prepare the training data
         for cur_state, prev_state, action, reward in mini_batch:
-            target = reward + self.gamma * np.amax(self.model.predict(cur_state)[0])
-            target_f = self.model.predict(prev_state)
+            target = reward + self.gamma * np.amax(self.prediction_model.predict(cur_state)[0])
+            target_f = self.prediction_model.predict(prev_state)
             target_f[0][action] = target
             # Add example to train
             X.append(prev_state.reshape(80, 80, 1))
@@ -58,10 +56,10 @@ class PongAgent(object):
         X = np.array(X)
         y = np.array(y)
 
-        self.model.fit(x=X, y=y, epochs=10)
+        self.prediction_model.fit(x=X, y=y, epochs=1)
 
     def choose_next_action(self, cur_state):
-        act_values = self.model.predict(cur_state)[0]
+        act_values = self.prediction_model.predict(cur_state)[0]
         action = np.argmax(act_values)
         q_value = np.max(act_values)
 
@@ -74,11 +72,11 @@ class PongAgent(object):
     def build_deep_rl_model(self):
 
         model = keras.models.Sequential()
-        model.add(keras.layers.convolutional.Conv2D(filters = 2, kernel_size=(2, 2), strides=1, padding='same', activation='relu', input_shape=(80, 80, 1)))
-        model.add(keras.layers.convolutional.MaxPooling2D(pool_size=(2,2)))
+        #model.add(keras.layers.convolutional.Conv2D(filters = 2, kernel_size=(2, 2), strides=1, padding='same', activation='relu', input_shape=(80, 80, 1)))
+        #model.add(keras.layers.convolutional.MaxPooling2D(pool_size=(2,2)))
         model.add(keras.layers.Flatten())
-        model.add(keras.layers.Dense(units = 40, activation='relu'))
-        model.add(keras.layers.Dense(units = 20, activation='relu'))
+        model.add(keras.layers.Dense(units = 160, activation='relu'))
+        model.add(keras.layers.Dense(units = 80, activation='relu'))
         model.add(keras.layers.Dense(units = self.num_actions, activation='sigmoid'))
 
         model.compile(optimizer='adam', loss='mse')
@@ -87,39 +85,44 @@ class PongAgent(object):
 
     def train(self):
         episodes = 0
-        gamma = 1
+        gamma = 0.6
 
-        prev_frame = np.zeros(shape=(80,80))
-        cur_frame = self.frame_preprocessing(self.env.reset())
-        prev_state = None
-        cur_state = (cur_frame - prev_frame).reshape((-1, 80, 80, 1))
-
+        state = self.frame_preprocessing(self.env.reset())
 
         while(episodes < self.max_episodes):
-            self.env.render()
+            self.env.render()#render a frame
 
+            # Sample an action
             if np.random.rand() <= self.epsilon:
-                action =  random.randrange(self.num_actions)
+                action = random.randrange(self.num_actions)
             else:
-                action, q_value = self.choose_next_action(cur_state)
+                action, q_value = self.choose_next_action(state.reshape(-1, 80, 80, 1))
 
             observation, reward, done, info = self.env.step(action)
+            # Merge three consecutive frames to capture movement
+            reward_sum = reward
+            next_state = self.frame_preprocessing(observation)
 
-            prev_frame = cur_frame
-            cur_frame = self.frame_preprocessing(observation)
-            prev_state = cur_state
-            cur_state = (cur_frame - prev_frame).reshape((-1, 80, 80, 1))
+            for _ in range(self.frames_to_merge - 1):
+                observation, reward, done, info = self.env.step(0)
+                reward_sum += reward
+                next_state += self.frame_preprocessing(observation)
+                self.env.render()  # render a frame
+
+            avg_reward = reward_sum/self.frames_to_merge
+            #toimage(next_state).show()
 
             if done: #episode (one game) is terminated
                 episodes += 1
-                cur_frame = self.frame_preprocessing(self.env.reset())
                 self.learn_from_experience()
+                state = self.frame_preprocessing(self.env.reset())
             else:
-                train_sample = (cur_state, prev_state, action, reward)
+                state = next_state
+                train_sample = (next_state.reshape(-1, 80, 80, 1), state.reshape(-1, 80, 80, 1), action, avg_reward)
                 self.update_experience_buffer(train_sample)
 
         self.env.close()
-
+        self.prediction_model.save_weights("prediction_model_weights.h5")
 
 # Main function to control the agent
 def main():
